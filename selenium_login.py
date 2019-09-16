fd6bc2b23e362aa7d67dbfdb1e31d274f6600671
anna1-wq <nnbrams@gmail.com>
2019-09-16 15:42:23 +0300

    task3

--- selenium_login.py
+++ selenium_login.py
@@ -4,9 +4,9 @@ import time
 browser = webdriver.Chrome()
 browser.get("http://localhost:8081/litecard/public_html/admin/login.php")
 try:
-    usr_name_field= browser.find_element_by_xpath("//form[@name='login_form']/div[1]/table/tbody/tr[1]/td[2]/span/input")
+    usr_name_field = browser.find_element_by_name("username")
     usr_name_field.send_keys('admin')
-    paswd_field = browser.find_element_by_xpath("//form[@name='login_form']/div[1]/table/tbody/tr[2]/td[2]/span/input")
+    paswd_field = browser.find_element_by_name("password")
     paswd_field.send_keys('admin')
     button_login = browser.find_element_by_tag_name('button')
     button_login.click()
